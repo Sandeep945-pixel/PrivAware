00b2f62d8c8e3f684e99b4e1379a738c20a86ac1
Sandeep945-pixel <98046375+Sandeep945-pixel@users.noreply.github.com>
2026-03-18 18:02:13 -0400

    Update MongoDB client connection string
    Removed sensitive MongoDB connection string.

--- db/db.py
+++ db/db.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient
 from core.config import MONGO_URI
 
 
-client = MongoClient("mongodb+srv://sahithipadidala:rahul@llmprivacy.fzefy.mongodb.net/?retryWrites=true&w=majority&appName=LLMPrivacy")
+client = MongoClient("")
 db = client["LLM_Privacy_updated"]
 collection = db["Privacy_updated"]
 input_response_collection = db["Input_response"]
